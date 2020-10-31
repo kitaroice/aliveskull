58d6b1df4b42ce6ae13521a18758550e9999d206
ayoubsan <52720209+ayoubsan@users.noreply.github.com>
2020-10-31 15:54:43 +0100

    1

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client 
+
+const token = "NzcxOTEzMDA0OTY2MDg0NjI5.X5zCAQ.Nu2dV_uAYEFoRbCB_M6MgsBPsJA"
+
+bot.on('ready', () =>{
+    console.log('Bot online!')
+    bot.user.setActivity('Minecraft')
+
+})
+
+    bot.on('message', msg=>{
+        if(msg.content === "Hi"){
+            msg.channel.send('Hi I will go now')
+        }
+    })
+
+
+bot.login(token)
